9f73bbc6beeb35375079ffc6c4569f67a4c37638
Steffen Ritter <info@rs-websystems.de>
2011-10-23 09:03:24 +0200

    [TASK] first individual options
    Change-Id: Idc73b542a5cf073419da0060e98ec905f4e6730a

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,36 +6,15 @@ if (!defined('TYPO3_MODE')) {
 	// Add mapping of Media domain model to tables
 $tableMapping = '
 config.tx_extbase.persistence.classes {
-	Tx_Dam_Domain_Model_Media.mapping.tableName = tx_media
-	Tx_Dam_Domain_Model_Text.mapping.tableName = tx_media
-	Tx_Dam_Domain_Model_Image.mapping.tableName = tx_media
-	Tx_Dam_Domain_Model_Audio.mapping.tableName = tx_media
-	Tx_Dam_Domain_Model_Video.mapping.tableName = tx_media
-	Tx_Dam_Domain_Model_Software.mapping.tableName = tx_media
+	Tx_Dam_Domain_Model_Media.mapping.tableName = sys_file
+	Tx_Dam_Domain_Model_Text.mapping.tableName = sys_file
+	Tx_Dam_Domain_Model_Image.mapping.tableName = sys_file
+	Tx_Dam_Domain_Model_Audio.mapping.tableName = sys_file
+	Tx_Dam_Domain_Model_Video.mapping.tableName = sys_file
+	Tx_Dam_Domain_Model_Software.mapping.tableName = sys_file
 }
 ';
 t3lib_extMgm::addTypoScript('media', 'setup', $tableMapping);
-
-
-Tx_Extbase_Utility_Extension::configurePlugin(
-	$_EXTKEY,
-	'Frontend',
-	array(
-		'Media' => 'show, list, new, create, edit, update, delete',
-		'Collection' => 'list, show, new, create, edit, update, delete',
-		'Filter' => 'list, show, new, create, edit, update, delete',
-		'File' => 'show, list, new, create, edit, update, delete',
-		
-	),
-	// non-cacheable actions
-	array(
-		'Media' => 'create, update, delete',
-		'Collection' => 'create, update, delete',
-		'Filter' => 'create, update, delete',
-		'File' => 'create, update, delete',
-		
-	)
-);
 
 	// register special TCE tx_media processing
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:media/Classes/Hooks/TCE.php:&Tx_Media_Hooks_TCE';
