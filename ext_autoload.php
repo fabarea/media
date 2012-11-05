a9949c62fcffb0ee81bc60679d4458234d6f2034
Oliver Hader <oliver@typo3.org>
2012-11-05 13:56:45 +0100

    [TASK] Use ExtensionManagerUtility
    \TYPO3\CMS\\Core\Extension\ExtensionManager was renamed to
    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility in the Core.
    
    Change-Id: I987e3e213cb5eea376b677bb2180ed97e3d09101
    Related: #42661

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,5 +1,5 @@
 <?php
-$extensionPath = \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('extbase');
+$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('extbase');
 $extensionClassesPath = $extensionPath . 'Classes/';
 return array(
 	'Tx_Media_Controller_MediaController' => $extensionClassesPath . 'Controller/MediaController.php',
