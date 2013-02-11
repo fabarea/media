b32f7122c57eadc082816fd28af7d5d1dfc2cc14
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-02-11 16:53:08 +0100

    [TASK] Comment out unused declaration for the Object Manager
    Change-Id: I6f1cd01f71ef3e8a0bac85895d560d3cc1e7dd65
    Reviewed-on: https://review.typo3.org/18200
    Reviewed-by: Fabien Udriot
    Tested-by: Fabien Udriot

--- ext_localconf.php
+++ ext_localconf.php
@@ -21,7 +21,8 @@ config.tx_extbase.persistence.classes {
 #$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:media/Classes/Hooks/TCE.php:&Tx_Media_Hooks_TCE';
 
 // Override classes for the Object Manager
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Core\Resource\ResourceStorage'] =
-	array('className' => 'TYPO3\CMS\Media\Override\Core\Resource\ResourceStorage');
+// @todo can be removed I guess since used of add() method of FAL (and not addUploaded - or something like that)
+#$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Core\Resource\ResourceStorage'] =
+#	array('className' => 'TYPO3\CMS\Media\Override\Core\Resource\ResourceStorage');
 
 ?>
