9446a90dcab7623e5b0049174eef4a6ed245e740
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-12-12 10:37:40 +0400

    [FEATURE] Hide delete button if soft reference exists to Asset
    Change-Id: I3b3608c5e59dbaddccd97f80711a7629c782b50b
    Releases: 2.0.0
    Resolves: #54350
    Reviewed-on: https://review.typo3.org/26351
    Reviewed-by: Fabien Udriot
    Tested-by: Fabien Udriot

--- Classes/ViewHelpers/Component/ButtonDeleteViewHelper.php
+++ Classes/ViewHelpers/Component/ButtonDeleteViewHelper.php
@@ -23,12 +23,15 @@ namespace TYPO3\CMS\Media\ViewHelpers\Component;
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 use TYPO3\CMS\Backend\Utility\IconUtility;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3\CMS\Media\Utility\ModuleUtility;
+use TYPO3\CMS\Vidi\Domain\Model\Content;
+use TYPO3\CMS\Media\ObjectFactory;
 
 /**
  * View helper which renders a "delete" button to be placed in the grid.
  */
-class ButtonDeleteViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+class ButtonDeleteViewHelper extends AbstractViewHelper {
 
 	/**
 	 * Renders a "delete" button to be placed in the grid.
@@ -36,9 +39,12 @@ class ButtonDeleteViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractVi
 	 * @param \TYPO3\CMS\Vidi\Domain\Model\Content $object
 	 * @return string
 	 */
-	public function render(\TYPO3\CMS\Vidi\Domain\Model\Content $object = NULL) {
+	public function render(Content $object = NULL) {
 		$result = '';
-		if ($this->hasFileNoReferences($object)) {
+
+		$asset = ObjectFactory::getInstance()->convertContentObjectToAsset($object);
+
+		if ($this->hasFileNoReferences($object) && $this->hasNotSoftImageReferences($asset) && $this->hasNotSoftLinkReferences($asset)) {
 
 			// check if the file has a reference
 			$result = sprintf('<a href="%s&%s[asset]=%s" class="btn-delete" data-uid="%s">%s</a>',
@@ -55,7 +61,7 @@ class ButtonDeleteViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractVi
 	/**
 	 * Tell whether the file has references.
 	 *
-	 * @param \TYPO3\CMS\Vidi\Domain\Model\Content $object
+	 * @param Content $object
 	 * @return boolean
 	 */
 	protected function hasFileNoReferences($object) {
@@ -68,6 +74,47 @@ class ButtonDeleteViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractVi
 		);
 
 		return empty($references);
+	}
+
+	/**
+	 * Return whether the asset has no soft image references.
+	 *
+	 * @param \TYPO3\CMS\Media\Domain\Model\Asset $asset
+	 * @return array
+	 */
+	public function hasNotSoftImageReferences($asset) {
+		$softReferences = array();
+
+		foreach ($asset->getVariants() as $variant) {
+
+			// Get the file references of the asset.
+			$_softReferences = $this->getDatabaseConnection()->exec_SELECTgetRows(
+				'recuid, tablename',
+				'sys_refindex',
+				'deleted = 0 AND softref_key = "rtehtmlarea_images" AND ref_table = "sys_file" AND ref_uid = ' . $variant->getVariant()->getUid()
+			);
+
+			$softReferences = array_merge($softReferences, $_softReferences);
+		}
+		return empty($softReferences);
+	}
+
+	/**
+	 * Return whether the asset has no soft link references.
+	 *
+	 * @param \TYPO3\CMS\Media\Domain\Model\Asset $asset
+	 * @return array
+	 */
+	public function hasNotSoftLinkReferences($asset) {
+
+		// Get the link references of the asset.
+		$softReferences = $this->getDatabaseConnection()->exec_SELECTgetRows(
+			'recuid, tablename',
+			'sys_refindex',
+			'deleted = 0 AND softref_key = "typolink_tag" AND ref_table = "sys_file" AND ref_uid = ' . $asset->getUid()
+		);
+
+		return empty($softReferences);
 	}
 
 	/**
