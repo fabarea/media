793eb810b5ff72f0bb41b7fa3696dd5748140ba7
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-02-24 12:19:21 +0100

    [TASK] Update version number
    Change-Id: I6b5d2ec277315b3d08a93af275869428e4caff56

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array (
   'modify_tables' => '',
   'clearCacheOnLoad' => 0,
   'lockType' => '',
-  'version' => '2.0.1',
+  'version' => '2.0.2-dev',
   'constraints' => 
   array (
     'depends' => 
