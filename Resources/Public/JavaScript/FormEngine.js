1481dfb6c36a301f91959892cd36d6aa1fec3122
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-05-14 17:33:05 +0200

    [BUGFIX] Prevent Media Picker to show up widely
    The File Picker window should only pop up
    where relevant (and not everywhere as currently).
    
    This patch bring some discipline to the File Picker by checking
    if the popup is of type inline which means a file will be picked.
    
    Change-Id: Ib1fabeff0bc83d43fd4f746d4df41ff47ce2ba4b
    Releases: 3.0
    Resolves: #58812 #55506
    Reviewed-on: https://review.typo3.org/30130
    Reviewed-by: Fabien Udriot
    Tested-by: Fabien Udriot

--- Resources/Public/JavaScript/FormEngine.js
+++ Resources/Public/JavaScript/FormEngine.js
@@ -24,21 +24,31 @@ define('TYPO3/CMS/Media/FormEngine', ['jquery'], function ($) {
 	 * @param params additional params for the browser window
 	 */
 	FormEngine.openPopupWindow = setFormValueOpenBrowser = function(mode, params) {
+		var url;
 
-		// It was changed below from original implementation - Fabien - 11.11.2013
-		// An additional filter could be applied if required "&tx_vidi_user_vidisysfilem1[matches][type]=2"
-		var url = FormEngine.backPath + vidiModuleUrl + '&tx_vidi_user_vidisysfilem1[plugins][]=filePicker&params=' + params;
+		// Open the Media Picker if we encounter an inline segment in the params -> file to be selected.
+		if (params.search(/inline\./) > -1) {
 
-		//var name = "File Picker"; // Commented since IE compatibility issue. Weird!
-		var dimensions = {
-			top: 0,
-			left: 0,
-			width: 1280,
-			height: 800
-		};
+			// An additional filter could be applied if required "&tx_vidi_user_vidisysfilem1[matches][type]=2"
+			url = FormEngine.backPath + vidiModuleUrl + '&tx_vidi_user_vidisysfilem1[plugins][]=filePicker&params=' + params;
 
-		FormEngine.openedPopupWindow = window.open(url, "Typo3WinBrowser", "toolbar=no,location=no,directories=no,menubar=no,resizable=yes,top=" + dimensions.top + ",left=" + dimensions.left + ",dependent=yes,dialog=yes,chrome=no,width=" + dimensions.width + ",height=" + dimensions.height + ",scrollbars=yes");
-		FormEngine.openedPopupWindow.focus();
+			//var name = "File Picker"; // Commented since IE compatibility issue. Weird!
+			var dimensions = {
+				top: 0,
+				left: 0,
+				width: 1280,
+				height: 800
+			};
+
+			FormEngine.openedPopupWindow = window.open(url, "Typo3WinBrowser", "toolbar=no,location=no,directories=no,menubar=no,resizable=yes,top=" + dimensions.top + ",left=" + dimensions.left + ",dependent=yes,dialog=yes,chrome=no,width=" + dimensions.width + ",height=" + dimensions.height + ",scrollbars=yes");
+			FormEngine.openedPopupWindow.focus();
+
+		} else {
+			// Default record picker as we know it...
+			url = FormEngine.backPath + 'browser.php?mode=' + mode + '&bparams=' + params;
+			FormEngine.openedPopupWindow = window.open(url, 'Typo3WinBrowser', 'height=650,width=' + (mode == 'db' ? 650 : 600) + ',status=0,menubar=0,resizable=1,scrollbars=1');
+			FormEngine.openedPopupWindow.focus();
+		}
 	};
 
 });
