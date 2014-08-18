1a91a165e374f34ef6d85b829584b4a5caa4606a
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-08-18 09:50:58 +0200

    [TASK] Change copyright notice following Core change
    Change-Id: Ic8dfe2afa9c18a878231f3b3314b01e9ef30d4f6
    Release: 3.0
    Resolves: #61020
    Reviewed-on: http://review.typo3.org/32215
    Reviewed-by: Fabien Udriot <fabien.udriot@ecodev.ch>
    Tested-by: Fabien Udriot <fabien.udriot@ecodev.ch>

--- Classes/FileUpload/Optimizer/JpegExifOrient.php
+++ Classes/FileUpload/Optimizer/JpegExifOrient.php
@@ -1,32 +1,18 @@
 <?php
 namespace TYPO3\CMS\Media\FileUpload\Optimizer;
 
-/***************************************************************
- *  Copyright notice
+/**
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2013 Xavier Perseguers <xavier@causal.ch>
- *  based on work http://sylvana.net/jpegcrop/jpegexiforient.c
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *  A copy is found in the textfile GPL.txt and important notices to the license
- *  from the author is found in LICENSE.txt distributed with these scripts.
- *
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
 /**
  * This is a utility class to set the Exif Orientation Tag.
@@ -60,6 +46,7 @@ namespace TYPO3\CMS\Media\FileUpload\Optimizer;
  * 88          88      88  88
  * 88          88  888888  888888
  *
+ * @author 2013 Xavier Perseguers <xavier@causal.ch> based on work http://sylvana.net/jpegcrop/jpegexiforient.c
  */
 class JpegExifOrient {
 
