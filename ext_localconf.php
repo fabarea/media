44ea5c9086e29717fd4fc5373e333007f09f65ea
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-11-21 08:55:28 +0100

    [TASK] PSR-2 for files ext_localconf + ext_tables

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 // Register basic metadata extractor. Will feed the file with a "title" when indexing, e.g. upload, through scheduler
@@ -12,35 +12,34 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_extfilefunc.php']['
 
 if (TYPO3_MODE == 'BE') {
 
-	// Special process to fill column "usage" which indicates the total number of file reference including soft references.
-	$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'Fab\Media\Hook\DataHandlerHook';
+    // Special process to fill column "usage" which indicates the total number of file reference including soft references.
+    $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'Fab\Media\Hook\DataHandlerHook';
 
-	# Configuration for RTE
-	$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['LinkCreator'] = array();
-	$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['LinkCreator']['objectReference'] = 'EXT:media/Resources/Private/HtmlArea/LinkCreator/class.tx_rtehtmlarea_linkcreator.php:&tx_rtehtmlarea_linkcreator';
-	$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['LinkCreator']['addIconsToSkin'] = 1;
-	$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['LinkCreator']['disableInFE'] = 1;
+    # Configuration for RTE
+//    $TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['LinkCreator'] = array();
+//    $TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['LinkCreator']['objectReference'] = 'EXT:media/Resources/Private/HtmlArea/LinkCreator/class.tx_rtehtmlarea_linkcreator.php:&tx_rtehtmlarea_linkcreator';
+//    $TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['LinkCreator']['addIconsToSkin'] = 1;
+//    $TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['LinkCreator']['disableInFE'] = 1;
+//
+//    $TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['ImageEditor'] = array();
+//    $TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['ImageEditor']['objectReference'] = 'EXT:media/Resources/Private/HtmlArea/ImageEditor/class.tx_rtehtmlarea_imageeditor.php:&tx_rtehtmlarea_imageeditor';
+//    $TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['ImageEditor']['addIconsToSkin'] = 1;
+//    $TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['ImageEditor']['disableInFE'] = 1;
 
-	$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['ImageEditor'] = array();
-	$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['ImageEditor']['objectReference'] = 'EXT:media/Resources/Private/HtmlArea/ImageEditor/class.tx_rtehtmlarea_imageeditor.php:&tx_rtehtmlarea_imageeditor';
-	$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['ImageEditor']['addIconsToSkin'] = 1;
-	$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins']['ImageEditor']['disableInFE'] = 1;
+    // Setting up scripts that can be run from the cli_dispatch.phpsh script.
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Fab\Media\Command\FileCacheCommandController';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Fab\Media\Command\MissingFilesCommandController';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Fab\Media\Command\DuplicateFilesCommandController';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Fab\Media\Command\DuplicateRecordsCommandController';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Fab\Media\Command\ThumbnailCommandController';
 
-	// Setting up scripts that can be run from the cli_dispatch.phpsh script.
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Fab\Media\Command\FileCacheCommandController';
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Fab\Media\Command\MissingFilesCommandController';
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Fab\Media\Command\DuplicateFilesCommandController';
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Fab\Media\Command\DuplicateRecordsCommandController';
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Fab\Media\Command\ThumbnailCommandController';
+    $configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['media']);
+    $hasMediaFilePicker = isset($configuration['has_media_file_picker']) ? $configuration['has_media_file_picker'] : FALSE;
+    if ($hasMediaFilePicker) {
 
-	$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['media']);
-	$hasMediaFilePicker = isset($configuration['has_media_file_picker']) ? $configuration['has_media_file_picker'] : FALSE;
-	if ($hasMediaFilePicker) {
-
-		// Override classes for the Object Manager.
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Backend\Form\FormEngine'] = array(
-			'className' => 'Fab\Media\Override\Backend\Form\FormEngine'
-		);
-	}
-
+        // Override classes for the Object Manager.
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Backend\Form\FormEngine'] = array(
+            'className' => 'Fab\Media\Override\Backend\Form\FormEngine'
+        );
+    }
 }
--- ext_tables.php
+++ ext_tables.php
@@ -1,20 +1,20 @@
 <?php
 
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 if (TYPO3_MODE == 'BE') {
 
-	/** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
-	$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
+    /** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
+    $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
 
-	/** @var \TYPO3\CMS\Extensionmanager\Utility\ConfigurationUtility $configurationUtility */
-	$configurationUtility = $objectManager->get('TYPO3\CMS\Extensionmanager\Utility\ConfigurationUtility');
-	$configuration = $configurationUtility->getCurrentConfiguration('media');
+    /** @var \TYPO3\CMS\Extensionmanager\Utility\ConfigurationUtility $configurationUtility */
+    $configurationUtility = $objectManager->get('TYPO3\CMS\Extensionmanager\Utility\ConfigurationUtility');
+    $configuration = $configurationUtility->getCurrentConfiguration('media');
 
-	// Default User TSConfig to be added in any case.
-	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
+    // Default User TSConfig to be added in any case.
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
 
 		# Enable or disabled the Media File Picker in the BE
 		options.vidi.enableMediaFilePicker = 1
@@ -24,23 +24,23 @@ if (TYPO3_MODE == 'BE') {
 
 	');
 
-	$moduleFileLanguage = 'LLL:EXT:media/Resources/Private/Language/locallang.xlf';
-	if ($configuration['hide_file_list']['value'] == 1) {
+    $moduleFileLanguage = 'LLL:EXT:media/Resources/Private/Language/locallang.xlf';
+    if ($configuration['hide_file_list']['value'] == 1) {
 
-		$moduleFileLanguage = 'LLL:EXT:media/Resources/Private/Language/locallang_filelist.xlf';
+        $moduleFileLanguage = 'LLL:EXT:media/Resources/Private/Language/locallang_filelist.xlf';
 
-		// Default User TSConfig to be added in any case.
-		TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
+        // Default User TSConfig to be added in any case.
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
 
 			# Hide default File List
 			options.hideModules.file := addToList(list)
 		');
-	}
+    }
 
-	// Possibly load additional User TSConfig.
-	if ($configuration['load_rte_configuration']['value'] == 1) {
+    // Possibly load additional User TSConfig.
+    if ($configuration['load_rte_configuration']['value'] == 1) {
 
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
 			RTE.default.showButtons := addToList(linkcreator,imageeditor)
 
 			// Toolbar order
@@ -50,147 +50,147 @@ if (TYPO3_MODE == 'BE') {
 			RTE.default.RTEHeightOverride = 700
 			RTE.default.RTEWidthOverride = 700
 		');
-	}
+    }
 
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-		'media',
-		'user', // Make media module a submodule of 'user'
-		'm1',
-		'bottom', // Position
-		array(
-			'Asset' => 'create, update, download, editStorage',
-			'ImageEditor' => 'show',
-			'LinkCreator' => 'show',
-			'ProcessedFile' => 'create',
-		),
-		array(
-			'access' => 'user,group',
-			'icon' => 'EXT:media/ext_icon.gif',
-			'labels' => 'LLL:EXT:media/Resources/Private/Language/locallang_module.xlf',
-		)
-	);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'media',
+        'user', // Make media module a submodule of 'user'
+        'm1',
+        'bottom', // Position
+        array(
+            'Asset' => 'create, update, download, editStorage',
+            'ImageEditor' => 'show',
+            'LinkCreator' => 'show',
+            'ProcessedFile' => 'create',
+        ),
+        array(
+            'access' => 'user,group',
+            'icon' => 'EXT:media/ext_icon.gif',
+            'labels' => 'LLL:EXT:media/Resources/Private/Language/locallang_module.xlf',
+        )
+    );
 
-	$defaultMainModule = (bool)$configuration['has_folder_tree']['value'] ? 'file' : 'content';
+    $defaultMainModule = (bool)$configuration['has_folder_tree']['value'] ? 'file' : 'content';
 
-	/** @var \Fab\Vidi\Module\ModuleLoader $moduleLoader */
-	$moduleLoader = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Fab\Vidi\Module\ModuleLoader', 'sys_file');
-	$moduleLoader->setIcon('EXT:media/ext_icon.gif')
-		->setModuleLanguageFile($moduleFileLanguage)
-		->setMainModule($defaultMainModule)
-		->addJavaScriptFiles(
-			array(
-				'EXT:media/Resources/Public/JavaScript/Initialize.js',
-				'EXT:media/Resources/Public/JavaScript/Media.js',
-				'EXT:media/Resources/Public/JavaScript/Media.EditStorage.js',
-				'EXT:media/Resources/Public/JavaScript/Media.BrowseRecursively.js',
-				'EXT:media/Resources/Public/Libraries/Fineuploader/jquery.fineuploader-5.0.9.min.js',
-			)
-		)
-		->addStyleSheetFiles(
-			array(
-				'EXT:media/Resources/Public/StyleSheets/media.css',
-				'EXT:media/Resources/Public/StyleSheets/fineuploader.css',
-			)
-		)
-		->setDocHeaderTopLeftComponents(
-			array(
-				'Fab\Media\View\Menu\StorageMenu',
-				'Fab\Media\View\Checkbox\RecursiveCheckbox'
-			)
-		)
-		->setDocHeaderBottomLeftComponents(
-			array(
-				'Fab\Vidi\View\Button\ClipboardButton',
-				'Fab\Media\View\Button\NewFolder',
-				'Fab\Media\View\Button\UploadButton',
-			)
-		)
-		->setGridTopComponents(
-			array(
-				'Fab\Media\View\InlineJavaScript',
-				'Fab\Media\View\Warning\ConfigurationWarning',
-				'Fab\Media\View\Info\SelectedFolderInfo',
-			)
-		)
-		->setGridBottomComponents(
-			array(
-				'Fab\Media\View\Plugin\LinkCreatorPlugin',
-				'Fab\Media\View\Plugin\ImageEditorPlugin',
-				'Fab\Media\View\Plugin\FilePickerPlugin',
-			)
-		)
-		->setGridButtonsComponents(
-			array(
-				'Fab\Media\View\Button\LinkCreatorButton',
-				'Fab\Media\View\Button\ImageEditorButton',
-				'Fab\Media\View\Button\FilePickerButton',
-				'Fab\Media\View\Button\EditButton',
-				'Fab\Media\View\Button\DownloadButton',
-				'Fab\Media\View\Button\DeleteButton',
-			)
-		)
-		->setMenuMassActionComponents(
-			array(
-				'Fab\Vidi\View\MenuItem\ExportXlsMenuItem',
-				'Fab\Vidi\View\MenuItem\ExportXmlMenuItem',
-				'Fab\Vidi\View\MenuItem\ExportCsvMenuItem',
-				'Fab\Vidi\View\MenuItem\DividerMenuItem',
+    /** @var \Fab\Vidi\Module\ModuleLoader $moduleLoader */
+    $moduleLoader = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Fab\Vidi\Module\ModuleLoader', 'sys_file');
+    $moduleLoader->setIcon('EXT:media/ext_icon.gif')
+        ->setModuleLanguageFile($moduleFileLanguage)
+        ->setMainModule($defaultMainModule)
+        ->addJavaScriptFiles(
+            array(
+                'EXT:media/Resources/Public/JavaScript/Initialize.js',
+                'EXT:media/Resources/Public/JavaScript/Media.js',
+                'EXT:media/Resources/Public/JavaScript/Media.EditStorage.js',
+                'EXT:media/Resources/Public/JavaScript/Media.BrowseRecursively.js',
+                'EXT:media/Resources/Public/Libraries/Fineuploader/jquery.fineuploader-5.0.9.min.js',
+            )
+        )
+        ->addStyleSheetFiles(
+            array(
+                'EXT:media/Resources/Public/StyleSheets/media.css',
+                'EXT:media/Resources/Public/StyleSheets/fineuploader.css',
+            )
+        )
+        ->setDocHeaderTopLeftComponents(
+            array(
+                'Fab\Media\View\Menu\StorageMenu',
+                'Fab\Media\View\Checkbox\RecursiveCheckbox'
+            )
+        )
+        ->setDocHeaderBottomLeftComponents(
+            array(
+                'Fab\Vidi\View\Button\ClipboardButton',
+                'Fab\Media\View\Button\NewFolder',
+                'Fab\Media\View\Button\UploadButton',
+            )
+        )
+        ->setGridTopComponents(
+            array(
+                'Fab\Media\View\InlineJavaScript',
+                'Fab\Media\View\Warning\ConfigurationWarning',
+                'Fab\Media\View\Info\SelectedFolderInfo',
+            )
+        )
+        ->setGridBottomComponents(
+            array(
+                'Fab\Media\View\Plugin\LinkCreatorPlugin',
+                'Fab\Media\View\Plugin\ImageEditorPlugin',
+                'Fab\Media\View\Plugin\FilePickerPlugin',
+            )
+        )
+        ->setGridButtonsComponents(
+            array(
+                'Fab\Media\View\Button\LinkCreatorButton',
+                'Fab\Media\View\Button\ImageEditorButton',
+                'Fab\Media\View\Button\FilePickerButton',
+                'Fab\Media\View\Button\EditButton',
+                'Fab\Media\View\Button\DownloadButton',
+                'Fab\Media\View\Button\DeleteButton',
+            )
+        )
+        ->setMenuMassActionComponents(
+            array(
+                'Fab\Vidi\View\MenuItem\ExportXlsMenuItem',
+                'Fab\Vidi\View\MenuItem\ExportXmlMenuItem',
+                'Fab\Vidi\View\MenuItem\ExportCsvMenuItem',
+                'Fab\Vidi\View\MenuItem\DividerMenuItem',
 
-				// Media custom View Helper
-				'Fab\Vidi\View\MenuItem\ClipboardMenuItem',
-				'Fab\Media\View\MenuItem\FilePickerMenuItem',
-				'Fab\Media\View\MenuItem\ChangeStorageMenuItem',
-				'Fab\Vidi\View\MenuItem\MassDeleteMenuItem',
-			)
-		)
-		->register();
+                // Media custom View Helper
+                'Fab\Vidi\View\MenuItem\ClipboardMenuItem',
+                'Fab\Media\View\MenuItem\FilePickerMenuItem',
+                'Fab\Media\View\MenuItem\ChangeStorageMenuItem',
+                'Fab\Vidi\View\MenuItem\MassDeleteMenuItem',
+            )
+        )
+        ->register();
 
-	/** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
-	$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
+    /** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
+    $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
 
-	/** @var $signalSlotDispatcher \TYPO3\CMS\Extbase\SignalSlot\Dispatcher */
-	$signalSlotDispatcher = $objectManager->get('TYPO3\CMS\Extbase\SignalSlot\Dispatcher');
+    /** @var $signalSlotDispatcher \TYPO3\CMS\Extbase\SignalSlot\Dispatcher */
+    $signalSlotDispatcher = $objectManager->get('TYPO3\CMS\Extbase\SignalSlot\Dispatcher');
 
-	# Register some tool for Media.
-	\Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\ThumbnailGeneratorTool');
-	\Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\CacheWarmUpTool');
-	\Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\MissingFilesFinderTool');
-	\Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\DuplicateRecordsFinderTool');
-	\Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\DuplicateFilesFinderTool');
+    # Register some tool for Media.
+    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\ThumbnailGeneratorTool');
+    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\CacheWarmUpTool');
+    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\MissingFilesFinderTool');
+    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\DuplicateRecordsFinderTool');
+    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\DuplicateFilesFinderTool');
 
-	// Connect some signals with slots.
-	$signalSlotDispatcher->connect(
-		'Fab\Vidi\Controller\Backend\ContentController',
-		'postProcessMatcherObject',
-		'Fab\Media\Security\FilePermissionsAspect',
-		'addFilePermissionsForFileStorages',
-		TRUE
-	);
+    // Connect some signals with slots.
+    $signalSlotDispatcher->connect(
+        'Fab\Vidi\Controller\Backend\ContentController',
+        'postProcessMatcherObject',
+        'Fab\Media\Security\FilePermissionsAspect',
+        'addFilePermissionsForFileStorages',
+        TRUE
+    );
 
-	$signalSlotDispatcher->connect(
-		'Fab\Vidi\Domain\Repository\ContentRepository',
-		'postProcessConstraintsObject',
-		'Fab\Media\Security\FilePermissionsAspect',
-		'addFilePermissionsForFileMounts',
-		TRUE
-	);
+    $signalSlotDispatcher->connect(
+        'Fab\Vidi\Domain\Repository\ContentRepository',
+        'postProcessConstraintsObject',
+        'Fab\Media\Security\FilePermissionsAspect',
+        'addFilePermissionsForFileMounts',
+        TRUE
+    );
 
-	$signalSlotDispatcher->connect(
-		'Fab\Vidi\Service\ContentService',
-		'afterFindContentObjects',
-		'Fab\Media\Facet\ActionPermissionFacet',
-		'modifyResultSet',
-		TRUE
-	);
+    $signalSlotDispatcher->connect(
+        'Fab\Vidi\Service\ContentService',
+        'afterFindContentObjects',
+        'Fab\Media\Facet\ActionPermissionFacet',
+        'modifyResultSet',
+        TRUE
+    );
 
 }
 
 \TYPO3\CMS\Backend\Sprite\SpriteManager::addSingleIcons(
-	array(
-		'image-edit' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/image_edit.png',
-		'image-link' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/image_link.png',
-		'image-export' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/image_export.png',
-		'storage-change' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/folder_go.png',
-	),
-	'media'
+    array(
+        'image-edit' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/image_edit.png',
+        'image-link' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/image_link.png',
+        'image-export' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/image_export.png',
+        'storage-change' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/folder_go.png',
+    ),
+    'media'
 );
