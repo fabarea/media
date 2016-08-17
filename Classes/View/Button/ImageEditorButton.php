09a7140039222a2657b3a79c13849619fda57156
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-08-17 16:16:08 +0200

    [TASK] Update copyright header

--- Classes/View/Button/ImageEditorButton.php
+++ Classes/View/Button/ImageEditorButton.php
@@ -1,17 +1,11 @@
 <?php
 namespace Fab\Media\View\Button;
 
-/**
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
+/*
+ * This file is part of the Fab/Media project under GPLv2 or later.
  *
  * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
+ * LICENSE.md file that was distributed with this source code.
  */
 
 use Fab\Media\Module\MediaModule;
