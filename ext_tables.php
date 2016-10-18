fabarea/media

commit 9132e8e9e06b2cf6186b770b9a12af0cad03e6af

[CGL] Add PHP annotations + get class name from class itself

ext_tables.php
@@ -4,10 +4,10 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-if (TYPO3_MODE == 'BE') {
+if (TYPO3_MODE === 'BE') {
 
     /** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
-    $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
+    $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\ObjectManager::class);
 
     /** @var \TYPO3\CMS\Extensionmanager\Utility\ConfigurationUtility $configurationUtility */
     $configurationUtility = $objectManager->get('TYPO3\CMS\Extensionmanager\Utility\ConfigurationUtility');
@@ -38,7 +38,7 @@ if (TYPO3_MODE == 'BE') {
     }
 
     // Possibly load additional User TSConfig.
-    if ($configuration['load_rte_configuration']['value'] == 1) {
+    if ((int)$configuration['load_rte_configuration']['value'] === 1) {
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
 			RTE.default.showButtons := addToList(linkcreator,imageeditor)
@@ -57,151 +57,138 @@ if (TYPO3_MODE == 'BE') {
         'user', // Make media module a submodule of 'user'
         'm1',
         'bottom', // Position
-        array(
+        [
             'Asset' => 'create, update, download, editStorage',
             'ImageEditor' => 'show',
             'LinkCreator' => 'show',
             'ProcessedFile' => 'create',
-        ),
-        array(
+        ],
+        [
             'access' => 'user,group',
             'icon' => 'EXT:media/ext_icon.gif',
             'labels' => 'LLL:EXT:media/Resources/Private/Language/locallang_module.xlf',
-        )
+        ]
     );
 
     $defaultMainModule = (bool)$configuration['has_folder_tree']['value'] ? 'file' : 'content';
 
     /** @var \Fab\Vidi\Module\ModuleLoader $moduleLoader */
-    $moduleLoader = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Fab\Vidi\Module\ModuleLoader', 'sys_file');
+    $moduleLoader = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \Fab\Vidi\Module\ModuleLoader::class,
+        'sys_file'
+    );
     $moduleLoader->setIcon('EXT:media/ext_icon.gif')
         ->setModuleLanguageFile($moduleFileLanguage)
         ->setMainModule($defaultMainModule)
-        ->addJavaScriptFiles(
-            array(
-                'EXT:media/Resources/Public/Libraries/Fineuploader/jquery.fineuploader-5.0.9.min.js',
-            )
+        ->addJavaScriptFiles([
+            'EXT:media/Resources/Public/Libraries/Fineuploader/jquery.fineuploader-5.0.9.min.js',
+        ])
+        ->addStyleSheetFiles([
+            'EXT:media/Resources/Public/StyleSheets/media.css',
+            'EXT:media/Resources/Public/StyleSheets/fineuploader.css',
+        ])
+        ->setDocHeaderTopLeftComponents([
+                \Fab\Media\View\Menu\StorageMenu::class,
+                \Fab\Media\View\Checkbox\RecursiveCheckbox::class,]
         )
-        ->addStyleSheetFiles(
-            array(
-                'EXT:media/Resources/Public/StyleSheets/media.css',
-                'EXT:media/Resources/Public/StyleSheets/fineuploader.css',
-            )
-        )
-        ->setDocHeaderTopLeftComponents(
-            array(
-                'Fab\Media\View\Menu\StorageMenu',
-                'Fab\Media\View\Checkbox\RecursiveCheckbox'
-            )
-        )
-        ->setDocHeaderBottomLeftComponents(
-            array(
-                'Fab\Vidi\View\Button\ClipboardButton',
-                'Fab\Media\View\Button\NewFolder',
-                'Fab\Media\View\Button\UploadButton',
-            )
-        )
-        ->setGridTopComponents(
-            array(
-                'Fab\Media\View\InlineJavaScript',
-                'Fab\Media\View\Warning\ConfigurationWarning',
-                'Fab\Media\View\Info\SelectedFolderInfo',
-            )
-        )
-        ->setGridBottomComponents(
-            array(
-                'Fab\Media\View\Plugin\LinkCreatorPlugin',
-                'Fab\Media\View\Plugin\ImageEditorPlugin',
-                'Fab\Media\View\Plugin\FilePickerPlugin',
-            )
-        )
-        ->setGridButtonsComponents(
-            array(
-                'Fab\Media\View\Button\LinkCreatorButton',
-                'Fab\Media\View\Button\ImageEditorButton',
-                'Fab\Media\View\Button\FilePickerButton',
-                'Fab\Media\View\Button\EditButton',
-                'Fab\Media\View\Button\DownloadButton',
-                'Fab\Media\View\Button\DeleteButton',
-            )
-        )
-        ->setMenuMassActionComponents(
-            array(
-                'Fab\Vidi\View\MenuItem\ExportXlsMenuItem',
-                'Fab\Vidi\View\MenuItem\ExportXmlMenuItem',
-                'Fab\Vidi\View\MenuItem\ExportCsvMenuItem',
-                'Fab\Vidi\View\MenuItem\DividerMenuItem',
+        ->setDocHeaderBottomLeftComponents([
+            \Fab\Vidi\View\Button\ClipboardButton::class,
+            \Fab\Media\View\Button\NewFolder::class,
+            \Fab\Media\View\Button\UploadButton::class,
+        ])
+        ->setGridTopComponents([
+            \Fab\Media\View\InlineJavaScript::class,
+            \Fab\Media\View\Warning\ConfigurationWarning::class,
+            \Fab\Media\View\Info\SelectedFolderInfo::class,
+        ])
+        ->setGridBottomComponents([
+            \Fab\Media\View\Plugin\LinkCreatorPlugin::class,
+            \Fab\Media\View\Plugin\ImageEditorPlugin::class,
+            \Fab\Media\View\Plugin\FilePickerPlugin::class,
+        ])
+        ->setGridButtonsComponents([
+            \Fab\Media\View\Button\LinkCreatorButton::class,
+            \Fab\Media\View\Button\ImageEditorButton::class,
+            \Fab\Media\View\Button\FilePickerButton::class,
+            \Fab\Media\View\Button\EditButton::class,
+            \Fab\Media\View\Button\DownloadButton::class,
+            \Fab\Media\View\Button\DeleteButton::class,
+        ])
+        ->setMenuMassActionComponents([
+            \Fab\Vidi\View\MenuItem\ExportXlsMenuItem::class,
+            \Fab\Vidi\View\MenuItem\ExportXmlMenuItem::class,
+            \Fab\Vidi\View\MenuItem\ExportCsvMenuItem::class,
+            \Fab\Vidi\View\MenuItem\DividerMenuItem::class,
 
-                // Media custom View Helper
-                'Fab\Vidi\View\MenuItem\ClipboardMenuItem',
-                'Fab\Media\View\MenuItem\FilePickerMenuItem',
-                'Fab\Media\View\MenuItem\ChangeStorageMenuItem',
-                'Fab\Vidi\View\MenuItem\MassDeleteMenuItem',
-            )
-        )
+            // Media custom View Helper
+            \Fab\Vidi\View\MenuItem\ClipboardMenuItem::class,
+            \Fab\Media\View\MenuItem\FilePickerMenuItem::class,
+            \Fab\Media\View\MenuItem\ChangeStorageMenuItem::class,
+            \Fab\Vidi\View\MenuItem\MassDeleteMenuItem::class,
+        ])
         ->register();
 
     /** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
-    $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
+    $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\ObjectManager::class);
 
     /** @var $signalSlotDispatcher \TYPO3\CMS\Extbase\SignalSlot\Dispatcher */
-    $signalSlotDispatcher = $objectManager->get('TYPO3\CMS\Extbase\SignalSlot\Dispatcher');
+    $signalSlotDispatcher = $objectManager->get(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
 
     # Register some tool for Media.
-    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\ThumbnailGeneratorTool');
-    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\CacheWarmUpTool');
-    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\MissingFilesFinderTool');
-    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\DuplicateRecordsFinderTool');
-    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', 'Fab\Media\Tool\DuplicateFilesFinderTool');
+    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', \Fab\Media\Tool\ThumbnailGeneratorTool::class);
+    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', \Fab\Media\Tool\CacheWarmUpTool::class);
+    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', \Fab\Media\Tool\MissingFilesFinderTool::class);
+    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', \Fab\Media\Tool\DuplicateRecordsFinderTool::class);
+    \Fab\Vidi\Tool\ToolRegistry::getInstance()->register('sys_file', \Fab\Media\Tool\DuplicateFilesFinderTool::class);
 
     // Connect some signals with slots.
     $signalSlotDispatcher->connect(
-        'Fab\Vidi\Controller\Backend\ContentController',
+        '\Fab\Vidi\Controller\Backend\ContentController',
         'postProcessMatcherObject',
-        'Fab\Media\Security\FilePermissionsAspect',
+        \Fab\Media\Security\FilePermissionsAspect::class,
         'addFilePermissionsForFileStorages',
-        TRUE
+        true
     );
 
     $signalSlotDispatcher->connect(
-        'Fab\Vidi\Domain\Repository\ContentRepository',
+        \Fab\Vidi\Domain\Repository\ContentRepository::class,
         'postProcessConstraintsObject',
-        'Fab\Media\Security\FilePermissionsAspect',
+        \Fab\Media\Security\FilePermissionsAspect::class,
         'addFilePermissionsForFileMounts',
-        TRUE
+        true
     );
 
     $signalSlotDispatcher->connect(
-        'Fab\Vidi\Service\ContentService',
+        \Fab\Vidi\Service\ContentService::class,
         'afterFindContentObjects',
-        'Fab\Media\Facet\ActionPermissionFacet',
+        \Fab\Media\Facet\ActionPermissionFacet::class,
         'modifyResultSet',
-        TRUE
+        true
     );
 
     // @bug Class property $relativePathToSkin does not look to be working anymore since TYPO3 7. Workaround: load CSS for the RTE as skin.
     // Reference EXT:media/Classes/Rtehtmlarea/Extension/LinkCreator.php
     if (is_file(PATH_site . 'typo3/sysext/rtehtmlarea/Resources/Public/JavaScript/Plugins/LinkCreator.js') ||
-        is_file(PATH_site . 'typo3/sysext/rtehtmlarea/Resources/Public/JavaScript/Plugins/ImageEditor.js')) {
+        is_file(PATH_site . 'typo3/sysext/rtehtmlarea/Resources/Public/JavaScript/Plugins/ImageEditor.js')
+    ) {
 
         // Register as a skin
-        $GLOBALS['TBE_STYLES']['skins']['media'] = array(
+        $GLOBALS['TBE_STYLES']['skins']['media'] = [
             'name' => 'media',
-            'stylesheetDirectories' => array(
+            'stylesheetDirectories' => [
                 'css' => 'EXT:media/Resources/Public/HtmlArea/'
-            )
-        );
+            ]
+        ];
     }
 }
 
 
-
 \TYPO3\CMS\Backend\Sprite\SpriteManager::addSingleIcons(
-    array(
+    [
         'image-edit' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/image_edit.png',
         'image-link' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/image_link.png',
         'image-export' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/image_export.png',
         'storage-change' => TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('media') . 'Resources/Public/Icons/folder_go.png',
-    ),
+    ],
     'media'
 );

Classes/Controller/AssetController.php
@@ -8,7 +8,12 @@ namespace Fab\Media\Controller;
  * LICENSE.md file that was distributed with this source code.
  */
 
+use Fab\Media\Cache\CacheService;
+use Fab\Media\FileUpload\UploadManager;
+use Fab\Media\Index\MediaIndexer;
 use Fab\Media\Module\MediaModule;
+use Fab\Media\ViewHelpers\MetadataViewHelper;
+use Fab\Vidi\Service\ContentService;
 use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\Resource\Exception\ExistingTargetFileNameException;
 use TYPO3\CMS\Core\Resource\Exception\IllegalFileExtensionException;
@@ -26,6 +31,7 @@ use Fab\Media\Thumbnail\ThumbnailInterface;
 use Fab\Media\Thumbnail\ThumbnailService;
 use Fab\Vidi\Persistence\MatcherObjectFactory;
 use Fab\Vidi\Tca\Tca;
+use TYPO3\CMS\Extbase\SignalSlot\Dispatcher;
 
 /**
  * Controller which handles actions related to Asset.
@@ -40,6 +46,8 @@ class AssetController extends ActionController
 
     /**
      * @throws \Fab\Media\Exception\StorageNotOnlineException
+     * @throws \InvalidArgumentException
+     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\NoSuchArgumentException
      */
     public function initializeAction()
     {
@@ -63,6 +71,7 @@ class AssetController extends ActionController
      * @param File $file
      * @param bool $forceDownload
      * @return bool|string
+     * @throws \RuntimeException
      */
     public function downloadAction(File $file, $forceDownload = FALSE)
     {
@@ -148,6 +157,8 @@ class AssetController extends ActionController
      *
      * @param File $file
      * @return string
+     * @throws \InvalidArgumentException
+     * @throws \RuntimeException
      */
     public function updateAction(File $file)
     {
@@ -233,12 +244,13 @@ class AssetController extends ActionController
      * Handle file upload.
      *
      * @return \Fab\Media\FileUpload\UploadedFileInterface|array
+     * @throws \InvalidArgumentException
      */
     protected function handleUpload()
     {
 
-        /** @var $uploadManager \Fab\Media\FileUpload\UploadManager */
-        $uploadManager = GeneralUtility::makeInstance('Fab\Media\FileUpload\UploadManager');
+        /** @var $uploadManager UploadManager */
+        $uploadManager = GeneralUtility::makeInstance(UploadManager::class);
 
         try {
             /** @var $result \Fab\Media\FileUpload\UploadedFileInterface */
@@ -251,22 +263,25 @@ class AssetController extends ActionController
     }
 
     /**
-     * @return \Fab\Media\ViewHelpers\MetadataViewHelper
+     * @return MetadataViewHelper
+     * @throws \InvalidArgumentException
      */
     protected function getMetadataViewHelper()
     {
-        return GeneralUtility::makeInstance('Fab\Media\ViewHelpers\MetadataViewHelper');
+        return GeneralUtility::makeInstance(MetadataViewHelper::class);
     }
 
     /**
      * @param File $file
      * @return ThumbnailService
+     * @throws \Fab\Media\Exception\InvalidKeyInArrayException
+     * @throws \InvalidArgumentException
      */
     protected function getThumbnailService(File $file)
     {
 
         /** @var $thumbnailService ThumbnailService */
-        $thumbnailService = GeneralUtility::makeInstance('Fab\Media\Thumbnail\ThumbnailService', $file);
+        $thumbnailService = GeneralUtility::makeInstance(ThumbnailService::class, $file);
         $thumbnailService->setAppendTimeStamp(TRUE)
             ->setOutputType(ThumbnailInterface::OUTPUT_IMAGE_WRAPPED);
         return $thumbnailService;
@@ -276,19 +291,21 @@ class AssetController extends ActionController
      * Get the instance of the Indexer service to update the metadata of the file.
      *
      * @param int|ResourceStorage $storage
-     * @return \Fab\Media\Index\MediaIndexer
+     * @return MediaIndexer
+     * @throws \InvalidArgumentException
      */
     protected function getMediaIndexer($storage)
     {
-        return GeneralUtility::makeInstance('Fab\Media\Index\MediaIndexer', $storage);
+        return GeneralUtility::makeInstance(MediaIndexer::class, $storage);
     }
 
     /**
-     * @return \Fab\Media\Cache\CacheService
+     * @return CacheService
+     * @throws \InvalidArgumentException
      */
     protected function getCacheService()
     {
-        return GeneralUtility::makeInstance('Fab\Media\Cache\CacheService');
+        return GeneralUtility::makeInstance(CacheService::class);
     }
 
     /**
@@ -296,6 +313,8 @@ class AssetController extends ActionController
      *
      * @param File $file
      * @return void
+     * @throws \TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotReturnException
+     * @throws \TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotException
      * @signal
      */
     protected function emitBeforeDownloadSignal(File $file)
@@ -306,29 +325,29 @@ class AssetController extends ActionController
     /**
      * Get the SignalSlot dispatcher.
      *
-     * @return \TYPO3\CMS\Extbase\SignalSlot\Dispatcher
+     * @return Dispatcher
      */
     protected function getSignalSlotDispatcher()
     {
-        return $this->objectManager->get('TYPO3\CMS\Extbase\SignalSlot\Dispatcher');
+        return $this->objectManager->get(Dispatcher::class);
     }
 
     /**
-     * Get the Vidi Module Loader.
-     *
-     * @return \Fab\Vidi\Service\ContentService
+     * @return ContentService
+     * @throws \InvalidArgumentException
      */
     protected function getContentService()
     {
-        return GeneralUtility::makeInstance('Fab\Vidi\Service\ContentService', $this->dataType);
+        return GeneralUtility::makeInstance(ContentService::class, $this->dataType);
     }
 
     /**
      * @return MediaModule
+     * @throws \InvalidArgumentException
      */
     protected function getMediaModule()
     {
-        return GeneralUtility::makeInstance('Fab\Media\Module\MediaModule');
+        return GeneralUtility::makeInstance(MediaModule::class);
     }
 
 }

Classes/View/Button/DeleteButton.php
@@ -24,6 +24,8 @@ class DeleteButton extends AbstractComponentView
      *
      * @param \Fab\Vidi\Domain\Model\Content $object
      * @return string
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
      */
     public function render(Content $object = NULL)
     {
@@ -53,6 +55,7 @@ class DeleteButton extends AbstractComponentView
     /**
      * @param Content $object
      * @return string
+     * @throws \InvalidArgumentException
      */
     protected function getDeleteUri(Content $object)
     {
@@ -71,6 +74,7 @@ class DeleteButton extends AbstractComponentView
 
     /**
      * @return \Fab\Media\Resource\FileReferenceService
+     * @throws \InvalidArgumentException
      */
     protected function getFileReferenceService()
     {
@@ -79,6 +83,7 @@ class DeleteButton extends AbstractComponentView
 
     /**
      * @return \Fab\Media\TypeConverter\ContentToFileConverter
+     * @throws \InvalidArgumentException
      */
     protected function getFileConverter()
     {

Classes/View/Button/DownloadButton.php
@@ -25,8 +25,9 @@ class DownloadButton extends AbstractComponentView
      *
      * @param Content $object
      * @return string
+     * @throws \InvalidArgumentException
      */
-    public function render(Content $object = NULL)
+    public function render(Content $object = null)
     {
 
         $button = $this->makeLinkButton()
@@ -49,14 +50,14 @@ class DownloadButton extends AbstractComponentView
      */
     protected function getDownloadUri(Content $object)
     {
-        $urlParameters = array(
-            MediaModule::getParameterPrefix() => array(
+        $urlParameters = [
+            MediaModule::getParameterPrefix() => [
                 'controller' => 'Asset',
                 'action' => 'download',
                 'forceDownload' => TRUE,
                 'file' => $object->getUid(),
-            ),
-        );
+            ],
+        ];
         return BackendUtility::getModuleUrl(MediaModule::getSignature(), $urlParameters);
     }
 

Classes/View/Checkbox/RecursiveCheckbox.php
@@ -29,6 +29,7 @@ class RecursiveCheckbox extends AbstractComponentView
      * Renders a checkbox for recursive file browsing.
      *
      * @return string
+     * @throws \InvalidArgumentException
      */
     public function render()
     {
@@ -44,6 +45,7 @@ class RecursiveCheckbox extends AbstractComponentView
 
     /**
      * @return string
+     * @throws \InvalidArgumentException
      */
     protected function isDisplayed()
     {
@@ -81,6 +83,7 @@ class RecursiveCheckbox extends AbstractComponentView
 
     /**
      * @return void
+     * @throws \InvalidArgumentException
      */
     protected function loadRequireJsCode()
     {
@@ -94,6 +97,7 @@ class RecursiveCheckbox extends AbstractComponentView
 
     /**
      * @return MediaModule
+     * @throws \InvalidArgumentException
      */
     protected function getMediaModule()
     {

commit 2046ab72a1e0f2c33f6d167f188623868a6b91cb

[BUGFIX] Fix registration of FilePermissionsAspect slot
This fixes a regression introduced with 9132e8e9e06b2cf6186b770b9a12af0cad03e6af.

Fixes #150

ext_tables.php
@@ -143,7 +143,7 @@ if (TYPO3_MODE === 'BE') {
 
     // Connect some signals with slots.
     $signalSlotDispatcher->connect(
-        'Fab\Vidi\Controller\Backend\ContentController',
+        \Fab\Vidi\Controller\Backend\ContentController::class,
         'postProcessMatcherObject',
         \Fab\Media\Security\FilePermissionsAspect::class,
         'addFilePermissionsForFileStorages'