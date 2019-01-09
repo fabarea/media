92652c112a5baec8affd6d2b35cd1d105f220570
Fabien Udriot <fabien@omic.ch>
2019-01-09 17:06:29 +0100

    Raise extension version

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'stable',
-    'version' => '4.6.0-dev',
+    'version' => '5.0.0-dev',
     'autoload' => [
         'psr-4' => ['Fab\\Media\\' => 'Classes']
     ],
