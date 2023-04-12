94286001a5147420f450980651ab8b9cdfd12bc2
Fabien Udriot <fabien@omic.ch>
2023-04-12 11:41:00 +0200

    [TASK] Remove unused code and dependencies from ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,28 +3,6 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Media management',
     'description' => 'Media management system for TYPO3 CMS.',
-    'category' => 'module',
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
-    'state' => 'stable',
-    'version' => '6.1.0-dev',
-    'autoload' => [
-        'psr-4' => ['Fab\\Media\\' => 'Classes']
-    ],
-    'constraints' =>
-        [
-            'depends' =>
-                [
-                    'typo3' => '11.5.0-11.5.99',
-                    'vidi' => '5.0.0-0.0.0',
-                ],
-            'conflicts' =>
-                [
-                ],
-            'suggests' =>
-                [
-                    'metadata' => '',
-                    'filemetadata' => '',
-                ],
-        ]
 ];
